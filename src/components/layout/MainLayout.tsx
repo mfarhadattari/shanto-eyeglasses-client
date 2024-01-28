fc6c9da71e9c8659de617bbb9494e90223abfae1
Mohammad Farhad <mfarhad.attari@gmail.com>
2024-01-28 08:51:31 +0600

    implemented main layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,11 +1,44 @@
+import { Avatar, Layout } from "antd";
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
+const { Header, Content } = Layout;
 
 const MainLayout = () => {
   return (
-    <div>
-      <h1>Main Layout</h1>
-      <Outlet />
-    </div>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sidebar />
+      <Layout>
+        <Header style={{ padding: 0 }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              height: "100%",
+              gap: "10px",
+              width: "100%",
+              justifyContent: "end",
+            }}
+          >
+            <Avatar
+              size={50}
+              src="/public/icon.png"
+              style={{ border: "2px solid white", marginRight: "20px" }}
+            />
+          </div>
+        </Header>
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            <Outlet />
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,87 @@
+import {
+  EyeOutlined,
+  HomeOutlined,
+  LogoutOutlined,
+  StockOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Button, Layout, Menu } from "antd";
+import { Link } from "react-router-dom";
+
+const { Sider } = Layout;
+
+const items = [
+  {
+    key: "/",
+    icon: <HomeOutlined />,
+    label: <Link to="/">Home</Link>,
+  },
+  {
+    key: "/eyeglasses",
+    icon: <EyeOutlined />,
+    label: <Link to="/eyeglasses">Eyeglasses</Link>,
+  },
+  {
+    key: "/sales",
+    icon: <StockOutlined />,
+    label: <Link to="/sales">Sales</Link>,
+  },
+  {
+    key: "/profile",
+    icon: <UserOutlined />,
+    label: <Link to="/my-profile">Profile</Link>,
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <Sider style={{ position: "relative" }} breakpoint="lg" collapsedWidth="0">
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "10px",
+        }}
+      >
+        <img
+          src="/public/icon.png"
+          alt="Eyeglasses Management"
+          style={{ height: "50px", width: "50px" }}
+        />
+        <h3
+          style={{
+            color: "white",
+            fontSize: "18px",
+            fontWeight: 500,
+            textTransform: "uppercase",
+          }}
+        >
+          Eyeglasses Management
+        </h3>
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["4"]}
+        items={items}
+      />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          position: "absolute",
+          bottom: "20px",
+        }}
+      >
+        <Button type="primary" danger>
+          <LogoutOutlined /> Logout
+        </Button>
+      </div>
+    </Sider>
+  );
+};
+
+export default Sidebar;
